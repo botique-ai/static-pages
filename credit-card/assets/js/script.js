db5127bf8e33fe7e00887c070deda46c8f704100
Netanel Gilad <netanel@botique.ai>
2017-12-07 11:38:17 +0200

    make credit card page return to bot

--- credit-card/assets/js/script.js
+++ credit-card/assets/js/script.js
@@ -1,62 +1,69 @@
-$(function() {
+$(function () {
 
-    var owner = $('#owner');
-    var cardNumber = $('#cardNumber');
-    var cardNumberField = $('#card-number-field');
-    var CVV = $("#cvv");
-    var mastercard = $("#mastercard");
-    var confirmButton = $('#confirm-purchase');
-    var visa = $("#visa");
-    var amex = $("#amex");
+  var owner = $('#owner');
+  var cardNumber = $('#cardNumber');
+  var cardNumberField = $('#card-number-field');
+  var CVV = $("#cvv");
+  var mastercard = $("#mastercard");
+  var confirmButton = $('#confirm-purchase');
+  var visa = $("#visa");
+  var amex = $("#amex");
 
-    // Use the payform library to format and validate
-    // the payment fields.
+  // Use the payform library to format and validate
+  // the payment fields.
 
-    cardNumber.payform('formatCardNumber');
-    CVV.payform('formatCardCVC');
+  cardNumber.payform('formatCardNumber');
+  CVV.payform('formatCardCVC');
 
 
-    cardNumber.keyup(function() {
+  cardNumber.keyup(function () {
 
-        amex.removeClass('transparent');
-        visa.removeClass('transparent');
-        mastercard.removeClass('transparent');
+    amex.removeClass('transparent');
+    visa.removeClass('transparent');
+    mastercard.removeClass('transparent');
 
-        if ($.payform.validateCardNumber(cardNumber.val()) == false) {
-            cardNumberField.addClass('has-error');
-        } else {
-            cardNumberField.removeClass('has-error');
-            cardNumberField.addClass('has-success');
-        }
+    if ($.payform.validateCardNumber(cardNumber.val()) == false) {
+      cardNumberField.addClass('has-error');
+    } else {
+      cardNumberField.removeClass('has-error');
+      cardNumberField.addClass('has-success');
+    }
 
-        if ($.payform.parseCardType(cardNumber.val()) == 'visa') {
-            mastercard.addClass('transparent');
-            amex.addClass('transparent');
-        } else if ($.payform.parseCardType(cardNumber.val()) == 'amex') {
-            mastercard.addClass('transparent');
-            visa.addClass('transparent');
-        } else if ($.payform.parseCardType(cardNumber.val()) == 'mastercard') {
-            amex.addClass('transparent');
-            visa.addClass('transparent');
-        }
+    if ($.payform.parseCardType(cardNumber.val()) == 'visa') {
+      mastercard.addClass('transparent');
+      amex.addClass('transparent');
+    } else if ($.payform.parseCardType(cardNumber.val()) == 'amex') {
+      mastercard.addClass('transparent');
+      visa.addClass('transparent');
+    } else if ($.payform.parseCardType(cardNumber.val()) == 'mastercard') {
+      amex.addClass('transparent');
+      visa.addClass('transparent');
+    }
+  });
+
+  function qs(key) {
+    key = key.replace(/[*+?^$.\[\]{}()|\\\/]/g, "\\$&"); // escape RegEx meta chars
+    var match = location.search.match(new RegExp("[?&]" + key + "=([^&]+)(&|$)"));
+    return match && decodeURIComponent(match[1].replace(/\+/g, " "));
+  }
+
+  confirmButton.click(function (e) {
+
+    e.preventDefault();
+
+    var userId = qs('userId');
+    var botId = qs('botId');
+    var returnURL = qs('returnURL');
+    var postback = qs('postback');
+
+    $.ajax({
+      url: returnURL + 'http/webhook/' + botId + '/' + userId,
+      type: 'POST',
+      data: JSON.stringify({postback: postback}),
+      contentType: 'application/json',
+      success: function () {
+        window.close();
+      }
     });
-
-    confirmButton.click(function(e) {
-
-        e.preventDefault();
-
-        var isCardValid = $.payform.validateCardNumber(cardNumber.val());
-        var isCvvValid = $.payform.validateCardCVC(CVV.val());
-
-        if(owner.val().length < 5){
-            alert("Wrong owner name");
-        } else if (!isCardValid) {
-            alert("Wrong card number");
-        } else if (!isCvvValid) {
-            alert("Wrong CVV");
-        } else {
-            // Everything is correct. Add your form submission code here.
-            alert("Everything is correct");
-        }
-    });
+  });
 });
